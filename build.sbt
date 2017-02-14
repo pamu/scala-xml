fbc506e7887fe179da5946e29a544d3cab04920d
pamu <p_nagarjuna@students.iitmandi.ac.in>
2017-02-14 12:16:05 +0530

    added untracked files

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "ScalaXML"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.scala-lang.modules" % "scala-xml_2.11" % "1.0.6"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,19 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    val weather =
+      <rss>
+        <channel>
+          <title>Yahoo! Weather - Boulder, CO</title>
+          <item>
+            <title>Conditions for Boulder, CO at 2:54 pm MST</title>
+            <forecast day="Thu" date="10 Nov 2011" low="37" high="58" text="Partly Cloudy"
+                      code="29" />
+          </item>
+        </channel>
+      </rss>
+
+    val forecast = weather \ "channel" \ "item" \ "forecast"
+
+    println(forecast)
+  }
+}
